06ace695c9a677da204ff200d65e062496a665e4
AYcrown77 <aycrown77@gmail.com>
2022-09-29 11:32:31 -0700

    Complete task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,9 +4,9 @@
 
 from sys import argv
 
-if __name == "__main__"
+if __name__ == "__main__"
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file =\
+    load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
 my_list = []
