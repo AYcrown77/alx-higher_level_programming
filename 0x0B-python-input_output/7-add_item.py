d9b7f35d02250684afdd2276ca856522cc8b75c3
AYcrown77 <aycrown77@gmail.com>
2022-09-29 05:18:50 -0700

    complete task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """My add_item module"""
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
 from sys import argv
+
+if __name == "__main__"
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file =\
+        __import__('6-load_from_json_file').load_from_json_file
 
 my_list = []
 filename = 'add_item.json'
