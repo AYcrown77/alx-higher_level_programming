a5cc3650ebbfe4f8ca2fe9dd558a3699b6c33369
AYcrown77 <aycrown77@gmail.com>
2022-09-29 11:56:52 -0700

    Complete task

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """My class_to_json module"""
-import json
 
 
 def class_to_json(obj):
-    """function that returns the dictionary description/
-       with simple data structure
+    """function that returns the dictionary description
     """
     return obj.__dict__
-
