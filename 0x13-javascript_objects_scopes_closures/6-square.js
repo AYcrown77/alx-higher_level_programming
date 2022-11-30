5cd0b4a3791f70ef344a302d13d42fe5edf5bcbc
AYcrown77 <aycrown77@gmail.com>
2022-11-30 11:35:25 +0000

    Complete task

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,21 +2,12 @@
 
 const baseSquare = require('./5-square');
 class Square extends baseSquare {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
-    if (c === null) {
-      this.print();
-    } else if (c === 'C') {
-      for (let i = 0; i < this.size; i++) {
-        let container = [];
-        for (let j = 0; j < this.size; j++) {
-          container += 'C';
-        }
-        console.log(container);
-      }
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
